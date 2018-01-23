1082e281d6727ff1de12fcfbefafa5ac9566779e
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-01-23 10:27:33 +0100

    Implemented loading files feature and several fixes

--- js/index.js
+++ js/index.js
@@ -376,7 +376,7 @@
           results.push(h++);
         }
         return results;
-      }, 50);
+      }, 350);
     });
   };
 
@@ -467,7 +467,6 @@
         $('#add-random').prop('disabled', true);
         $('#add-ir').prop('disabled', true);
         $('#add-camera').prop('disabled', true);
-        $('#save').prop('disabled', true);
         $('#validate').prop('disabled', true);
         return $('#stl').prop('disabled', true);
       }
@@ -534,7 +533,7 @@
         props[i].geometry = geometry.clone();
         props[i].rotation.x = -0.5 * Math.PI;
         props[i].rotation.z = 0;
-        props[i].position.set(16.5, 1, 0);
+        props[i].position.set(15.5, 0, 0);
         props[i].parent.rotation.y = angle - Math.PI / 6;
         results.push(i++);
       }
@@ -544,6 +543,8 @@
 
   renderer.domElement.addEventListener('mousedown', function(event) {
     var intersects, selected;
+    event.preventDefault();
+    console.log("CLICK");
     raycaster.setFromCamera(mouse, camera);
     //scene.remove ( arrow );
     //arrow = new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 100, Math.random() * 0xffffff )
@@ -572,8 +573,6 @@
       }
     }
   });
-
-  $('#save').click(function() {});
 
   toggleSidebar = function(name) {
     return $("#show-" + name + "-sidebar").click(function() {
@@ -718,7 +717,7 @@
   };
 
   saveDrone = function() {
-    var element, file, i, modules, modulesJSON, propsJSON, sceneJSON, str;
+    var cam_modulesJSON, custom_modulesJSON, element, file, i, ir_modulesJSON, propsJSON, sceneJSON, str;
     //TODO remove prompted modules
     sceneJSON = scene.toJSON();
     propsJSON = [];
@@ -727,17 +726,30 @@
       propsJSON.push(props[i].uuid);
       i += 1;
     }
-    modulesJSON = [];
-    modules = cam_modules.concat(ir_modules).concat(custom_modules);
+    cam_modulesJSON = [];
     i = 0;
-    while (i < modules.length) {
-      modulesJSON.push(props[i].uuid);
+    while (i < cam_modules.length) {
+      cam_modulesJSON.push(cam_modules[i].uuid);
+      i += 1;
+    }
+    ir_modulesJSON = [];
+    i = 0;
+    while (i < ir_modules.length) {
+      ir_modulesJSON.push(ir_modules[i].uuid);
+      i += 1;
+    }
+    custom_modulesJSON = [];
+    i = 0;
+    while (i < custom_modules.length) {
+      custom_modulesJSON.push(custom_modules[i].uuid);
       i += 1;
     }
     file = {
       scene: sceneJSON,
       props: propsJSON,
-      modules: modulesJSON
+      cam_modules: cam_modulesJSON,
+      ir_modules: ir_modulesJSON,
+      custom_modules: custom_modulesJSON
     };
     str = JSON.stringify(file);
     element = document.createElement('a');
@@ -750,39 +762,65 @@
 
   loadDrone = function(f) {
     var reader;
-    //TODO fix orbit controls issue
+    while (scene.children.length > 0) {
+      scene.remove(scene.children[0]);
+    }
+    console.log("LOADING");
     reader = new FileReader();
     reader.onload = function() {
-      var i, j, json, mod, modules, modulesArray, obj, propsArray;
+      var camArray, customArray, irArray, json, propsArray;
+      console.log("LOADED");
       json = JSON.parse(reader.result);
       loader = new THREE.ObjectLoader();
       scene = loader.parse(json.scene);
-      console.log(scene);
       propsArray = json.props;
-      modulesArray = json.modules;
+      camArray = json.cam_modules;
+      irArray = json.ir_modules;
+      customArray = json.custom_modules;
+      objects = [];
       props = [];
-      modules = [];
-      i = 0;
-      while (i < scene.children.length) {
-        obj = scene.children[i];
-        if (obj.type === "Object3D") {
-          j = 0;
-          while (j < obj.children.length) {
-            mod = obj.children[j];
-            console.log(mod.uuid);
-            if (propsArray.indexOf(mod.uuid) !== -1) {
-              props.push(mod);
-            } else if (modulesArray.indexOf(mod.uuid !== -1)) {
-              modules.push(mod);
+      cam_modules = [];
+      ir_modules = [];
+      custom_modules = [];
+      setTimeout(function() {
+        var i, j, mod, obj, results;
+        i = 0;
+        results = [];
+        while (i < scene.children.length) {
+          obj = scene.children[i];
+          if (obj.type === "Object3D") {
+            j = 0;
+            while (j < obj.children.length) {
+              mod = obj.children[j];
+              objects.push(mod);
+              console.log(mod.uuid);
+              if (propsArray.indexOf(mod.uuid) !== -1) {
+                props.push(mod);
+              } else if (camArray.indexOf(mod.uuid !== -1)) {
+                cam_modules.push(mod);
+              } else if (irArray.indexOf(mod.uuid !== -1)) {
+                ir_modules.push(mod);
+              } else if (customArray.indexOf(mod.uuid !== -1)) {
+                custom_modules.push(mod);
+              }
+              j += 1;
             }
-            j += 1;
           }
+          results.push(i += 1);
         }
-        i += 1;
-      }
+        return results;
+      }, 250);
+      loader = new THREE.STLLoader();
+      loader.crossOrigin = '';
+      raycaster = new THREE.Raycaster();
+      projector = new THREE.Projector();
       console.log(props);
-      return console.log(modules);
+      console.log(cam_modules);
+      console.log(ir_modules);
+      console.log(custom_modules);
+      return console.log(objects);
     };
+    reader.readAsText(f);
     return render();
   };
 
